d089476796e932584f32fbc3a3d120e8e0ebce1f
Michael Landon <michael@Michaels-MacBook-Pro.local>
2018-09-02 15:39:26 -0700

    add add, remove, and read functionality

--- notes.js
+++ notes.js
@@ -1,10 +1,69 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-	console.log('addnote');
-	return 'New note';
+const fs = require('fs');
+
+//Fetches notes from json an converts to object if the file exists, else returns an empty array
+//Empty array because if the file doesn't exist, it will be created with array on saveNotes
+var fetchNotes = () => {
+	try {
+		var notesString = fs.readFileSync('notes-data.json');
+		return JSON.parse(notesString);
+	} catch (e) {
+		return [];
+	}
 };
 
-module.exports.add = (a, b) => {
-	return a + b;
+//Accepts a notes object and overwrites or creates notes-data.json with a JSON string
+var saveNotes = (notes) => {
+	fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+var addNote = (title, body) => {
+	var notes = fetchNotes();
+	var note = {
+		title,
+		body
+	}
+	//Checks if a note by same title exists
+	var duplicateNotes = notes.filter( (note) => note.title === title);
+	//If note with same title doesn't exist, it adds and saves the note
+	if (duplicateNotes.length === 0) {
+		notes.push(note);
+		saveNotes(notes);
+		return note;
+	}
+};
+
+var getAll = () => {
+	console.log('Getting all notes...');
+};
+
+var getNote = (title) => {
+	var notes = fetchNotes();
+	var note = notes.filter( (note) => note.title === title);
+	return note[0];
+};
+
+var logNote = (note) => {
+	console.log('---');
+	console.log('Title:', note.title);
+	console.log('Body:', note.body);
+};
+
+var removeNote = (title) => {
+	var notes = fetchNotes();
+	//Creates a new array with all notes except one with title to be deleted
+	var filteredNotes = notes.filter( (note) => note.title !== title);
+	saveNotes(filteredNotes);
+	//If the lengths of notes before and after filtering differ, a note was deleted.
+	return notes.length !== filteredNotes.length;
 }
+
+//exports functions to be used in other files where this one is required
+module.exports = {
+	addNote,
+	getAll,
+	getNote,
+	removeNote,
+	logNote
+};
